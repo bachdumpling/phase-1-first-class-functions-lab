50f04a3215daefdebbfb4d0066b7af5fc926c780
Bach Le <lehoangbach7802@gmail.com>
2022-05-17 17:42:15 -0400

    commit tree 5b6495bfb40d979b67e22917d0ba70ac0b722aed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(drivers) {
+    return ['Antonia', 'Nuru'];
+}
+
+function returnLastTwoDrivers(drivers) {
+    return ['Amari', 'Mo'];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = (number) => {
+    return function(ridePrice) {
+        return ridePrice * number
+    }
+}
+
+const fareDoubler = function createFareMultiplier(number) {
+    return number * 2
+}
+
+const fareTripler = function createFareMultiplier(number) {
+    return number * 3
+}
+
+function selectDifferentDrivers(drivers, func) {
+    return func;
+}
